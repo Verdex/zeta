2edf6fdddeec725d1b60230523f603b8ed303448
David L. Inman <davidlevere@gmail.com>
2018-05-01 23:59:41 -0400

    console interact code

--- ConsoleInteract.fs
+++ ConsoleInteract.fs
@@ -1,13 +1,21 @@
 
 namespace Zeta
 
+module ConsoleTypes =
+
+    open Microsoft.FSharp.Control
+
+    type KeyPressEvent = KeyPressEvent of char * long
+    type InterfaceMessage = PostKey of KeyPressEvent 
+                          | GetNKeys of AsyncReplyChannel
+
 module ConsoleInterface =
 
     open Microsoft.FSharp.Control
 
     // TODO will need some sort of way to clean out the characters without
     // removing them before all the processes have used them
-    let private x = 0 
+    let mutable private x = 0 
 
 
 module ConsoleReader =
@@ -16,7 +24,7 @@ module ConsoleReader =
     open System
 
 
-    let initConsoleReader () = 
+    let initConsoleReader () =  // TODO init function takes console interface channel as param?
         let thread = new Thread( fun () -> let c = Console.ReadKey()
                                            Console.WriteLine( "blah " + new string([|c.KeyChar|])))
         thread.Start()

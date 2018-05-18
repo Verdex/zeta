9f2fa57db50755b7a95d5112536785da66c3729d
David L. Inman <davidlevere@gmail.com>
2018-05-17 22:06:04 -0400

    animator init

---
+++ Animation.fs
@@ -0,0 +1,13 @@
+
+namespace Zeta
+
+module Animator = 
+
+    open ConsoleType
+    open ConsoleInterface
+    open Microsoft.FSharp.Control
+
+    //let mutable private 
+
+    let initAnimator (consoleMailbox : MailboxProcessor<ConsoleMessage>) =   ()
+        
